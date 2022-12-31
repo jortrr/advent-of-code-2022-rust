6c5c4bcc71a23bdf869fd43552eb42a9cca22eb5
Jort vanWaes <JRR.vanWaes@student.han.nl>
2022-12-31 15:34:23 +0100

    Completed Day 9 part 2

--- day_9_rope_bridge/src/main.rs
+++ day_9_rope_bridge/src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::HashSet;
+use std::hash::{Hash, Hasher};
 use std::io::{BufRead, Write};
 
 fn main() {
@@ -23,7 +24,6 @@ fn main() {
     while reader.read_line(&mut line).unwrap() > 0 {
         //Remove trailing new-line character
         line = line.trim().to_string();
-        //Part 1
         PositionGrid::print_header(&line[..]);
         let direction = line.chars().nth(0).unwrap();
         let iterations = &line[2..];
@@ -32,16 +32,34 @@ fn main() {
         let iterations: u32 = iterations.parse().unwrap();
         for _ in 0..iterations {
             position_grid.head.go(&direction);
-            position_grid.tail.move_to(&position_grid.head);
+            //Part 1
+            position_grid.tail.move_to(&position_grid.head.position);
             position_grid.update_grid_corners(&position_grid.head.position.clone());
             position_grid.update_grid_corners(&position_grid.tail.position.clone());
-            position_grid.print_grid();
+            //position_grid.print_grid();
+            //Part 2
+            let mut head_knot_position: Position = position_grid.head.position;
+            for knot in &mut position_grid.nine_tail_knots {
+                knot.move_to(&head_knot_position);
+                head_knot_position = knot.position;
+            }
+            for i in 0..position_grid.nine_tail_knots.len() {
+                position_grid.update_grid_corners(
+                    &position_grid
+                        .nine_tail_knots
+                        .get(i)
+                        .unwrap()
+                        .position
+                        .clone(),
+                );
+            }
+            position_grid.print_grid_ten_knots();
         }
 
         line.clear(); //Clear line string
     }
     //Part 1
-    position_grid.print_tail_positions();
+    position_grid.print_visited_positions(&position_grid.tail, "Tail");
     writeln!(
         output_1_file,
         "{}",
@@ -49,7 +67,18 @@ fn main() {
     )
     .unwrap();
     //Part 2
-    writeln!(output_2_file, "{}", "To do").unwrap();
+    position_grid.print_visited_positions(&position_grid.nine_tail_knots.last().unwrap(), "9 Knot");
+    writeln!(
+        output_2_file,
+        "{}",
+        position_grid
+            .nine_tail_knots
+            .last()
+            .unwrap()
+            .visited_positions
+            .len()
+    )
+    .unwrap();
 }
 
 enum Direction {
@@ -78,20 +107,20 @@ struct Knot {
 }
 
 impl Knot {
-    fn new() -> Knot {
+    fn new(symbol: char) -> Knot {
         let mut result = Knot {
-            position: Position { x: 0, y: 0 },
+            position: Position { x: 0, y: 0, symbol },
             visited_positions: HashSet::new(),
         };
         result.visited_positions.insert(result.position); //Add starting Position to visited_positions
         result
     }
 
-    ///Move this Tail knot to the Head knot, according to the movement described in the advent of code assignment.
+    ///Move this Tail knot to the Head knot position, according to the movement described in the advent of code assignment.
     /// Returns the new Position of the Knot.
-    fn move_to(&mut self, head: &Knot) -> Position {
-        let delta_x = head.position.x - self.position.x;
-        let delta_y = head.position.y - self.position.y;
+    fn move_to(&mut self, head_position: &Position) -> Position {
+        let delta_x = head_position.x - self.position.x;
+        let delta_y = head_position.y - self.position.y;
         if delta_x == 0 && delta_y == 0 {
             //Head and Tail overlap, Tail does not need to move
             return self.position;
@@ -125,10 +154,33 @@ impl Knot {
     }
 }
 
-#[derive(Eq, Hash, PartialEq, Clone, Copy, Debug)] //Derive these traits so Position can be used in a HashSet
+#[derive(Eq, Clone, Copy, Debug)] //Derive these traits so Position can be used in a HashSet
 struct Position {
     x: i32,
     y: i32,
+    symbol: char,
+}
+
+impl Position {
+    fn new(x: i32, y: i32, symbol: char) -> Position {
+        Position { x, y, symbol }
+    }
+}
+
+impl PartialEq for Position {
+    //Implement PartialEq to te able to compare Positions purely based on their x and y values
+    fn eq(&self, other: &Self) -> bool {
+        self.x == other.x && self.y == other.y
+    }
+}
+
+impl Hash for Position {
+    //Implement Hash to be able to use it in HashSets purely by considering the x and y values of a Position.
+
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.x.hash(state);
+        self.y.hash(state);
+    }
 }
 
 ///A struct allowing us to maintain our required awareness of the head and tail Knots, and which allows us to print
@@ -137,6 +189,7 @@ struct PositionGrid {
     starting_position: Position,
     head: Knot,
     tail: Knot,
+    nine_tail_knots: [Knot; 9],
     grid_corner_bottom_left: Position,
     grid_corner_top_right: Position,
 }
@@ -144,11 +197,22 @@ struct PositionGrid {
 impl PositionGrid {
     fn new() -> PositionGrid {
         PositionGrid {
-            starting_position: Position { x: 0, y: 0 },
-            head: Knot::new(),
-            tail: Knot::new(),
-            grid_corner_bottom_left: Position { x: 0, y: 0 },
-            grid_corner_top_right: Position { x: 0, y: 0 },
+            starting_position: Position::new(0, 0, 's'),
+            head: Knot::new('H'),
+            tail: Knot::new('T'),
+            nine_tail_knots: [
+                Knot::new('1'),
+                Knot::new('2'),
+                Knot::new('3'),
+                Knot::new('4'),
+                Knot::new('5'),
+                Knot::new('6'),
+                Knot::new('7'),
+                Knot::new('8'),
+                Knot::new('9'),
+            ],
+            grid_corner_bottom_left: Position::new(0, 0, '.'),
+            grid_corner_top_right: Position::new(0, 0, '.'),
         }
     }
 
@@ -156,24 +220,48 @@ impl PositionGrid {
     /// an 'H' at the head, a 'T' at the tail, and '.'s at unoccupied positions.
     ///Make sure to update the grid corners by calling update_grid_corners() first.
     fn print_grid(&self) {
+        let positions = [
+            &self.head.position,
+            &self.tail.position,
+            &self.starting_position,
+        ];
+        self.print_grid_with_symbols(&positions[..]);
+    }
+
+    ///Prints the PositionGrid in the format specified in the Advent of Code assignment. Prints an 's' and the starting_position,
+    /// an 'H' at the head, '1'..'9' for the tail, and '.'s at unoccupied positions.
+    ///Make sure to update the grid corners by calling update_grid_corners() first.
+    fn print_grid_ten_knots(&self) {
+        //Add all Positions with a symbol to positions
+        let mut positions: Vec<&Position> = vec![&self.head.position];
+        for knot in &self.nine_tail_knots {
+            positions.push(&knot.position);
+        }
+        positions.push(&self.starting_position);
+        self.print_grid_with_symbols(&positions[..]);
+    }
+
+    ///Prints the PositionGrid in the format specified in the Advent of Code assignment. Prints the symbol associated with the Positions in
+    ///positions.
+    ///The order of a Position in positions determines which Position gets printed when Positions overlap, earlier Positions will get printed over later Positions,
+    /// when their coordinates are the same.
+    ///Make sure to update the grid corners by calling update_grid_corners() first.
+    fn print_grid_with_symbols(&self, positions: &[&Position]) {
         for y in (self.grid_corner_bottom_left.y..self.grid_corner_top_right.y + 1).rev() {
             for x in self.grid_corner_bottom_left.x..self.grid_corner_top_right.x + 1 {
-                let grid_position = Position { x, y };
-                //println!("grid_position = {:?}", grid_position);
-                let mut position_symbol = ".";
-                if grid_position == self.starting_position {
-                    position_symbol = "s";
-                }
-                if grid_position == self.tail.position {
-                    position_symbol = "T";
-                }
-                if grid_position == self.head.position {
-                    position_symbol = "H";
+                let grid_position = Position::new(x, y, '.');
+                let mut position_symbol = grid_position.symbol;
+                for position in positions {
+                    if grid_position == **position {
+                        position_symbol = position.symbol;
+                        break;
+                    }
                 }
                 print!("{}", position_symbol);
             }
+            println!();
             //Print a message when H, T or s are covered by eachother on the corresponding line, else print a newline.
-            if self.starting_position == self.tail.position && y == self.starting_position.y {
+            /*if self.starting_position == self.tail.position && y == self.starting_position.y {
                 if self.head.position == self.tail.position {
                     println!("\t(H covers T, s)");
                 } else {
@@ -183,30 +271,32 @@ impl PositionGrid {
                 println!("\t(H covers T)");
             } else {
                 println!();
-            }
+            }*/
+            //TODO
         }
         println!();
     }
 
-    ///Prints the PositionGrid with a '#' symbol at every Position in tail.visited_positions. Prints a 's' at starting_position.
-    fn print_tail_positions(&self) {
-        println!("Positions the Tail visited:");
+    ///Prints the PositionGrid with a '#' symbol at every Position in knot.visited_positions. Prints a 's' at starting_position.
+    fn print_visited_positions(&self, knot: &Knot, knot_name: &str) {
+        println!("Positions the {} visited:", knot_name);
         for y in (self.grid_corner_bottom_left.y..self.grid_corner_top_right.y + 1).rev() {
             for x in self.grid_corner_bottom_left.x..self.grid_corner_top_right.x + 1 {
-                let grid_position = Position { x, y };
-                let mut position_symbol = ".";
+                let grid_position = Position::new(x, y, '.');
+                let mut position_symbol = grid_position.symbol;
                 if grid_position == self.starting_position {
-                    position_symbol = "s";
-                } else if self.tail.visited_positions.contains(&grid_position) {
-                    position_symbol = "#"
+                    position_symbol = self.starting_position.symbol;
+                } else if knot.visited_positions.contains(&grid_position) {
+                    position_symbol = '#';
                 }
                 print!("{}", position_symbol);
             }
             println!();
         }
         println!(
-            "The Tail visited {} Positions.",
-            self.tail.visited_positions.len()
+            "The {} visited {} Positions.",
+            knot_name,
+            knot.visited_positions.len()
         );
     }
 
