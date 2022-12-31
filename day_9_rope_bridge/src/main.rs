c336c810e42dc32e8fec328c6798936a184e42e2
Jort vanWaes <JRR.vanWaes@student.han.nl>
2022-12-31 11:47:27 +0100

    Day 9 part 1 work in progress, print_grid() seems to work

--- day_9_rope_bridge/src/main.rs
+++ day_9_rope_bridge/src/main.rs
@@ -17,9 +17,25 @@ fn main() {
     let mut line = String::new();
     //---End---
 
+    let mut position_grid: PositionGrid = PositionGrid::new();
+    PositionGrid::print_header("Initial State");
+    position_grid.print_grid();
     while reader.read_line(&mut line).unwrap() > 0 {
-        line = line.trim().to_string(); //Remove trailing new-line character
-                                        //Do stuff
+        //Remove trailing new-line character
+        line = line.trim().to_string();
+        //Part 1
+        PositionGrid::print_header(&line[..]);
+        let direction = line.chars().nth(0).unwrap();
+        let iterations = line.chars().nth(2).unwrap();
+        //Convert the direction and iterations chars to the right types
+        let direction: Direction = Direction::new(direction);
+        let iterations: u32 = iterations.to_digit(10).unwrap();
+        for _ in 0..iterations {
+            position_grid.head.go(&direction);
+            position_grid.tail.move_to(&position_grid.head);
+            position_grid.update_grid_corners();
+            position_grid.print_grid();
+        }
 
         line.clear(); //Clear line string
     }
@@ -29,21 +45,21 @@ fn main() {
     writeln!(output_2_file, "{}", "To do").unwrap();
 }
 
-enum Motion {
+enum Direction {
     Up,
     Down,
     Left,
     Right,
 }
 
-impl Motion {
-    fn new(c: char) -> Motion {
+impl Direction {
+    fn new(c: char) -> Direction {
         match c {
-            'U' => Motion::Up,
-            'D' => Motion::Down,
-            'L' => Motion::Left,
-            'R' => Motion::Right,
-            _ => panic!("The character '{}' is not a valid Motion, c should be an element of {{'U', 'D', 'L', 'R'}}.", c),
+            'U' => Direction::Up,
+            'D' => Direction::Down,
+            'L' => Direction::Left,
+            'R' => Direction::Right,
+            _ => panic!("The character '{}' is not a valid Direction, c should be an element of {{'U', 'D', 'L', 'R'}}.", c),
         }
     }
 }
@@ -87,18 +103,18 @@ impl Knot {
     }
 
     ///Take 1 step in the given direction
-    fn go(&mut self, direction: &Motion) {
+    fn go(&mut self, direction: &Direction) {
         match direction {
-            Motion::Up => self.position.y += 1,
-            Motion::Down => self.position.y -= 1,
-            Motion::Left => self.position.x -= 1,
-            Motion::Right => self.position.x += 1,
+            Direction::Up => self.position.y += 1,
+            Direction::Down => self.position.y -= 1,
+            Direction::Left => self.position.x -= 1,
+            Direction::Right => self.position.x += 1,
         }
         self.visited_positions.insert(self.position);
     }
 }
 
-#[derive(Eq, Hash, PartialEq, Clone, Copy)] //Derive these traits so Position can be used in a HashSet
+#[derive(Eq, Hash, PartialEq, Clone, Copy, Debug)] //Derive these traits so Position can be used in a HashSet
 struct Position {
     x: i32,
     y: i32,
@@ -110,6 +126,8 @@ struct PositionGrid {
     starting_position: Position,
     head: Knot,
     tail: Knot,
+    grid_corner_bottom_left: Position,
+    grid_corner_top_right: Position,
 }
 
 impl PositionGrid {
@@ -118,14 +136,73 @@ impl PositionGrid {
             starting_position: Position { x: 0, y: 0 },
             head: Knot::new(),
             tail: Knot::new(),
+            grid_corner_bottom_left: Position { x: 0, y: 0 },
+            grid_corner_top_right: Position { x: 0, y: 0 },
         }
     }
 
+    ///Prints the PositionGrid in the format specified in the Advent of Code assignment. Prints an 's' and the starting_position,
+    /// an 'H' at the head, a 'T' at the tail, and '.'s at unoccupied positions.
+    ///Make sure to update the grid corners by calling update_grid_corners() first.
     fn print_grid(&self) {
-        //TODO
+        for y in (self.grid_corner_bottom_left.y..self.grid_corner_top_right.y + 1).rev() {
+            for x in self.grid_corner_bottom_left.x..self.grid_corner_top_right.x + 1 {
+                let grid_position = Position { x, y };
+                //println!("grid_position = {:?}", grid_position);
+                let mut position_symbol = ".";
+                if grid_position == self.starting_position {
+                    position_symbol = "s";
+                }
+                if grid_position == self.tail.position {
+                    position_symbol = "T";
+                }
+                if grid_position == self.head.position {
+                    position_symbol = "H";
+                }
+                print!("{}", position_symbol);
+            }
+            if self.starting_position == self.tail.position && y == self.starting_position.y {
+                if self.head.position == self.tail.position {
+                    println!("\t(H covers T, s)");
+                } else {
+                    println!("\t(T covers s)");
+                }
+            } else if self.head.position == self.tail.position && y == self.head.position.y {
+                println!("\t(H covers T)");
+            } else {
+                println!();
+            }
+        }
+        println!();
     }
 
     fn print_tail_positions(&self) {
         //TODO
+    }
+
+    ///Find the extreme values of the visited_positions of the Head and Tail knot, so we can know the size of the grid we need to draw.
+    ///Updates grid_corner_bottom_right and grid_corner_top_left.
+    fn update_grid_corners(&mut self) {
+        let mut x_min = 0;
+        let mut x_max = 0;
+        let mut y_min = 0;
+        let mut y_max = 0;
+        let knots = [&self.head, &self.tail];
+        for knot in knots {
+            for position in knot.visited_positions.iter() {
+                x_min = x_min.min(position.x);
+                x_max = x_max.max(position.x);
+                y_min = y_min.min(position.y);
+                y_max = y_max.max(position.y);
+            }
+        }
+        self.grid_corner_bottom_left = Position { x: x_min, y: y_min };
+        self.grid_corner_top_right = Position { x: x_max, y: y_max };
+    }
+
+    ///Print a PositionGrid header to the terminal in the following format: '== {header_text} =='
+    fn print_header(header_text: &str) {
+        println!("== {} ==", header_text);
+        println!();
     }
 }
